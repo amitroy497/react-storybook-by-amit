585f6ad38b46d4fe52f3474b4a63306087075a52
Amit Roy <amitroy497@yahoo.com>
2021-06-29 10:04:03 +0530

    first commit

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { addDecorator } from '@storybook/react'
+import Center from '../src/components/Center/Center'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
+}
+
+addDecorator((story) => <Center>{story()}</Center>)
